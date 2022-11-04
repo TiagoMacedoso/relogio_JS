9e7cbcfe8e44fddd258bbe28f627b5a068efb75c
Tiago Macedo Cardoso <contato.cardosotiago@gmail.com>
2022-11-04 09:46:19 -0300

    Create: Conclusão do front-end do projeto e também sua lógica

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+const currentTime = () => {
+    const horas = document.getElementById('hh');
+    const minutos = document.getElementById('mm');
+    const segundos = document.getElementById('ss');
+
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    hh = hh < 10 ? `0${hh}` : hh;
+    mm = mm < 10 ? `0${mm}` : mm;
+    ss = ss < 10 ? `0${ss}` : ss;
+
+    horas.textContent = hh;
+    minutos.textContent = mm;
+    segundos.textContent = ss;
+}
+
+currentTime();
+setInterval(currentTime, 1000);
